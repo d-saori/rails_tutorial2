e302149e77fe12920947adcb936a8c8590f4850c
d-saori <60836449+d-saori@users.noreply.github.com>
2020-07-20 16:55:44 +0900

    userモデルとmicropostモデルの関連付け

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,6 @@
 class Micropost < ApplicationRecord
+  # 1つのマイクロポストは1人のユーザーにのみ属する
+  belongs_to :user
+  # 最大140文字までのバリデーション
   validates :content, length: {maximum:140}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  # 1人のユーザーに複数のマイクロポストがある
+  has_many :microposts
 end
