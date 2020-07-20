ad16b2cefb160d013a794f78a057e812a502c8fd
d-saori <60836449+d-saori@users.noreply.github.com>
2020-07-20 17:10:04 +0900

    バリデーション追加

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,5 +2,7 @@ class Micropost < ApplicationRecord
   # 1つのマイクロポストは1人のユーザーにのみ属する
   belongs_to :user
   # 最大140文字までのバリデーション
-  validates :content, length: {maximum:140}
+  validates :content, length: {maximum:140},
+              # マイクロポストのコンテンツが存在しているかどうかの確認
+              presence: true
 end
